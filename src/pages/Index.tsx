6a8fe458244c688911181a90fe7a351293f351de
fatsirotem <wamit.rotem@gmail.com>
2026-01-05 23:01:01 +0200

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { supabase } from "@/lib/supabase";
 import Header from '@/components/Header';
 import AddVacationForm from '@/components/AddVacationForm';
 import VacationCalendar from '@/components/VacationCalendar';
